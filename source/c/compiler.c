695508b6127f665a693bb9a03c31269137385914
Emanuel Alvgard <emanuelalvgard@Emanuels-MacBook-Pro.local>
2020-12-03 22:21:53 +0100

    -

--- source/c/compiler.c
+++ source/c/compiler.c
@@ -14,52 +14,63 @@ char state = 0;
 
 
 
-void variable_name()
-{
-    while(1)
-    {
-        if (state == 0)
-        {
-            break;
-        }
-        else if ((code[character] > 96 && code[character] < 123) || code[character] == 95)
-        {
-            printf("%d", character);
-            character += 1;
-        }
-        else if (code[character] == 58 && code[character + 1] == 32)
-        {
-            printf("%d%d", character, character + 1);
-            state = 1;
-            character += 1;
-            break;
-        }
-        else
-        {
-            syntax_error = 1;
-            break;
-        }
-    }
+void new_line() {
+    if (code[character] == '\n') {
+        character += 1; 
+        } 
     return;
-}
-
-
-
-int main()
-{
-    while(syntax_error == 0)
-    {
-        variable_name();
     }
-    if (syntax_error == 0)
-    {
-        printf("success!\n");
-    }
-    else
-    {
-        printf("error!\n");
-    }            
-}
+
+
+
+void case_snake() {
+    while(1) {
+        if (code[character] > 96 && code[character] < 123) {
+            character += 1; }
+        else if (code[character] == 95) {
+            character += 1; }
+        else if () {
+            if (code[character] == 58 && code[character + 1] == 32) {
+            state = 1;
+            character += 2;
+            break; } }
+        else {
+            syntax_error = 1;
+            break; } }
+    return; }
+
+
+
+// STATES
+// 0 = keyword / symbol
+// 1 = assignment
+// 2 = declaration
+// 3 = definition
+
+// TYPES
+// case_snake
+// operator_assignment
+// operator_arithmetic
+// literal_number
+// literal_string
+// keyword_procedure
+
+
+
+int main() {
+    while (code[character] != 0 || syntax_error == 0) {
+        // indentation
+        // new_line
+        // keywords
+        case_snake();
+        // case_camel
+    
+        printf("%d", character); }
+
+    if (syntax_error == 0) {
+        printf("success!\n"); }
+    else {
+        printf("error!\n"); } }
 
 
 
