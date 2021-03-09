aba7e5b106f554ee2d15752df39a8247995298cb
emanuel-alvgard <46920077+emanuel-alvgard@users.noreply.github.com>
2021-03-09 15:25:36 +0100

    Delete src/javascript directory

--- src/javascript/wasm.js
+++
@@ -1,9 +0,0 @@
-const file = await Deno.open("output.wasm", { read: true });
-const wasm = await Deno.readAll(file);
-Deno.close(file.rid);
-
-const wasm_module = new WebAssembly.Module(wasm);
-const wasm_instance = new WebAssembly.Instance(wasm_module);
-console.log(wasm_instance.exports.main());
-
-//build the 'lite'-compiler in js on top of Deno. 
