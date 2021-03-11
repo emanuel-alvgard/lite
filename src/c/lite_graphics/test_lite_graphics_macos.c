96f5d35e9e41260097305a2e890f96cf78bfd21d
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-03-11 07:04:20 +0100

    -

--- src/c/lite_graphics/test_lite_graphics_macos.c
+++ src/c/lite_graphics/test_lite_graphics_macos.c
@@ -1,6 +1,13 @@
 #include "lite_graphics_macos.h"
 
 
+
+int main() {
+    return 0;
+}
+
+
+/*
 enum {
         RECT_X = 20,
         RECT_Y = 20,
@@ -13,6 +20,8 @@ enum {
         WIN_HEIGHT = 100,
         WIN_BORDER = 1
 };
+
+
 
 int main()
 {
@@ -37,24 +46,24 @@ int main()
         WhitePixel(display, screen)
         );
 
-        /* process window close event through event handler so XNextEvent does not fail */
+        /* process window close event through event handler so XNextEvent does not fail 
         Atom del_window = XInternAtom(display, "WM_DELETE_WINDOW", 0);
         XSetWMProtocols(display, window, &del_window, 1);
 
-        /* select kind of events we are interested in */
+        /* select kind of events we are interested in 
         XSelectInput(display, window, ExposureMask | KeyPressMask);
 
-        /* display the window */
+        /* display the window 
         XMapWindow(display, window);
 
-        /* event loop */
+        /* event loop 
         while (1) {
                 XNextEvent(display, &event);
 
                 if (event.type == KeyPress) {}
                 else if (event.type == ClientMessage) { break; }
                 else if (event.type == Expose) {
-                    /* draw the window */
+                    /* draw the window 
                     XFillRectangle(
                         display, 
                         window, 
@@ -67,11 +76,12 @@ int main()
                 }
         }
 
-        /* destroy window */
+        /* destroy window 
         XDestroyWindow(display, window);
 
-        /* close connection to server */
+        /* close connection to server 
         XCloseDisplay(display);
 
     return 0;
 }
+*/
